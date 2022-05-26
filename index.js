54cdff0b63426e1a55b45dbbb1b6ae7774483cb0
zamanmayed1 <zamanmayed02@gmail.com>
2022-05-26 18:33:52 +0600

    Allow Cors

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 // middleware
-app.use(cors())
+app.use(
+    cors({
+        origin: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
 app.use(express.json())
 app.use(bodyParser.json())
 
