f40659fa590fd5a9aa7605f153ba429657c42834
zamanmayed1 <zamanmayed02@gmail.com>
2022-05-30 19:16:03 +0600

    Try to solve heroku

--- index.js
+++ index.js
@@ -8,43 +8,34 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 // middleware
-app.use(cors());
-// const corsConfig = {
-//     origin: '*',
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE']
-// }
-// app.use(cors(corsConfig))
-// app.options("*", cors(corsConfig))
-// app.use(express.json())
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin",
-//         "*")
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept,authorization")
-//     next()
-// })
+app.use(cors({
+    origin: "https://stronghub-9986c.web.app/",
+}));
 
 
-function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: "UnAuthorized access" });
-    }
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-        if (err) {
-            console.log(err);
-            return res.status(403).send({ message: "Forbidden access" });
-        }
-        req.decoded = decoded;
-        next();
-    });
-}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3dogvsv.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthrized access' })
+    }
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbiden Access Bro' })
+        }
+        req.decoded = decoded
+        next()
+    });
+}
+
 
 async function run() {
     try {
@@ -60,9 +51,7 @@ async function run() {
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email
-            console.log(email);
             const user = req.body
-            console.log(user);
             const filter = { email: email }
             const options = { upsert: true };
             const updateDoc = {
@@ -70,7 +59,6 @@ async function run() {
             };
             const result = await Usercollection.updateOne(filter, updateDoc, options);
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            console.log(token);
             res.send({ result, token })
         })
 
@@ -92,25 +80,31 @@ async function run() {
         // Admin Check
         app.get('/admin/:email', async (req, res) => {
             const email = req.params.email
+            console.log(email);
             const user = await Usercollection.findOne({ email: email })
-            const isadmin = user?.role === 'admin'
-            res.send({ admin: isadmin })
-
+            const isAdmin = user.role == 'admin'
+            res.send({ admin: isAdmin })
         })
-
-        // make admn api
-        app.put('/user/admin/:email', async (req, res) => {
+        // make admin
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email
-            const filter = { email: email }
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: { role: 'admin' }
-            };
-            const result = await Usercollection.updateOne(filter, updateDoc);
-            res.send(result)
+
+            const requester = req.decoded.email
+            const requesterAccount = await Usercollection.findOne({ email: requester })
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email }
+                const options = { upsert: true };
+                const updateDoc = {
+                    $set: { role: 'admin' }
+                };
+                const result = await Usercollection.updateOne(filter, updateDoc);
+                res.send(result)
+            }
+            else {
+                res.status(403).send({ message: 'Forbiden access Boss' })
+            }
 
         })
-
         // Get All USers
         app.get('/users', async (req, res) => {
             const query = {};
